1d80758e3d790f551b90008f7ce0da0e2b55fe16
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2012-11-14 03:27:34 +0200

    Added unit test and some useful comments

--- src/main/java/com/lagodiuk/ga/Fitness.java
+++ src/main/java/com/lagodiuk/ga/Fitness.java
@@ -2,6 +2,13 @@ package com.lagodiuk.ga;
 
 public interface Fitness<G extends Gene<G>, T extends Comparable<T>> {
 
-	T calculate( G gene ); 
-	
+	/**
+	 * Assume that gene1 is better than gene2 <br/>
+	 * fit1 = calculate(gene1) <br/>
+	 * fit2 = calculate(gene2) <br/>
+	 * So the following condition must be true <br/>
+	 * fit1.compareTo(fit2) <= 0 <br/>
+	 */
+	T calculate(G gene);
+
 }
