f200b876afe594fdd50e49c632ec67823302f859
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2012-12-07 03:10:59 +0200

    Reduce default population size

--- src/main/java/com/lagodiuk/ga/Population.java
+++ src/main/java/com/lagodiuk/ga/Population.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Population<C extends Chromosome<C>> implements Iterable<C> {
 
-	private static final int DEFAULT_NUMBER_OF_CHROMOSOMES = 128;
+	private static final int DEFAULT_NUMBER_OF_CHROMOSOMES = 32;
 
 	private List<C> chromosomes = new ArrayList<C>(DEFAULT_NUMBER_OF_CHROMOSOMES);
 
