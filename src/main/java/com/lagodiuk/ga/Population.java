917c060321943177ab9e209ade0961a41b7d26af
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-01-16 16:29:10 +0200

    minor fix

--- src/main/java/com/lagodiuk/ga/Population.java
+++ src/main/java/com/lagodiuk/ga/Population.java
@@ -5,12 +5,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class Population<C extends Chromosome<C>> implements Iterable<C> {
 
 	private static final int DEFAULT_NUMBER_OF_CHROMOSOMES = 32;
 
 	private List<C> chromosomes = new ArrayList<C>(DEFAULT_NUMBER_OF_CHROMOSOMES);
+
+	private final Random random = new Random();
 
 	public void addChromosome(C chromosome) {
 		this.chromosomes.add(chromosome);
@@ -24,7 +27,7 @@ public class Population<C extends Chromosome<C>> implements Iterable<C> {
 		int numOfChromosomes = this.chromosomes.size();
 		// TODO improve random generator
 		// maybe use pattern strategy ?
-		int indx = (int) (Math.random() * numOfChromosomes);
+		int indx = this.random.nextInt(numOfChromosomes);
 		return this.chromosomes.get(indx);
 	}
 
